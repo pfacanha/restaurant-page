e6f3a4a942cc9683237a17ba0d47e1f54534d90d
pfacanha <pfacanha.dev@gmail.com>
2026-01-06 10:00:01 -0800

    Add css-loader + html-loader so webpack can recognize .css files and images path inside the html and bundle them appropriately

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.html$/i,
         loader: "html-loader",
       },
