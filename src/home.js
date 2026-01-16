7c05f9d732de86250486516d7bfee1382c899ec1
pfacanha <pfacanha.dev@gmail.com>
2026-01-16 10:55:57 -0800

    2nd test on exporting DOM elements from home.js to index.js

--- src/home.js
+++ src/home.js
@@ -1,14 +1,18 @@
 const message = console.log("home.js file was exported");
 
 // fetch elements
-const content = document.querySelector("#content");
+export function container() {
+  const content = document.querySelector("#content");
 
-// create elements
-const article = document.createElement("article");
-const h2 = document.createElement("h2");
-const p = document.createElement("p");
+  // create elements
+  const article = document.createElement("article");
+  const h2 = document.createElement("h2");
+  const p = document.createElement("p");
 
-article.append(h2, p);
-content.appendChild(article);
+  article.append(h2, p);
+  content.appendChild(article);
 
-export { content, message };
+  return content;
+}
+
+export { container, message };
