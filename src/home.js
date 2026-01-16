9d918aecde69b623470f2ecfb5f148d446b8b653
pfacanha <pfacanha.dev@gmail.com>
2026-01-16 10:47:32 -0800

    Work on home.js being exported to index.js

--- src/home.js
+++ src/home.js
@@ -1,3 +1,9 @@
-// export default console.log("I'm home.js");
+console.log("home.js file was exported");
 
+// create elements
 const article = document.createElement("article");
+const h2 = document.createElement("h2");
+const p = document.createElement("p");
+
+// append to article
+article.append(h2, p);
