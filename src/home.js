923b6dc15f9b918df4ad7a2c326c3997c00ca7f2
pfacanha <pfacanha.dev@gmail.com>
2026-01-16 10:51:07 -0800

    Test home.js

--- src/home.js
+++ src/home.js
@@ -1,9 +1,14 @@
-console.log("home.js file was exported");
+const message = console.log("home.js file was exported");
+
+// fetch elements
+const content = document.querySelector("#content");
 
 // create elements
 const article = document.createElement("article");
 const h2 = document.createElement("h2");
 const p = document.createElement("p");
 
-// append to article
 article.append(h2, p);
+content.appendChild(article);
+
+export { article, message };
