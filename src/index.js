6a8ad229808cb8ea5699e0d0c5073f2ddab44255
pfacanha <pfacanha.dev@gmail.com>
2026-01-17 10:40:06 -0800

    First test for event on aboutBtn

--- src/index.js
+++ src/index.js
@@ -6,3 +6,11 @@ console.log("index.js was loaded");
 
 const content = document.querySelector("#content");
 content.appendChild(homeScreen());
+
+// add event listener if about.js page is clicked
+const aboutBtn = document.querySelector(".about");
+aboutBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  console.log(`${aboutBtn.textContent} page was clicked!`);
+});
