85134c8e76f237b27f6e889b3b837e1bed1eb991
pfacanha <pfacanha.dev@gmail.com>
2026-01-22 20:04:25 -0800

    Fix minor typo in webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
 
-  devTool: "eval-source-map",
+  devtool: "eval-source-map",
 
   entry: "./src/index.js",
   output: {
